1fbc97ed2851421c47264b0f41b2ac40af1880ad
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2024-04-29 00:12:40 -0400

    MI

--- release/calc.d
+++
@@ -1,1 +0,0 @@
-/home/dotm87/Documentos/SCESI/postulacion/calc-scesi/calc/target/release/calc: /home/dotm87/Documentos/SCESI/postulacion/calc-scesi/calc/src/main.rs
--- release/deps/calc-17eb7921b3232d53.d
+++
@@ -1,5 +0,0 @@
-/home/dotm87/Documentos/SCESI/postulacion/calc-scesi/calc/target/release/deps/calc-17eb7921b3232d53: src/main.rs
-
-/home/dotm87/Documentos/SCESI/postulacion/calc-scesi/calc/target/release/deps/calc-17eb7921b3232d53.d: src/main.rs
-
-src/main.rs:
